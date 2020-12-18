2b13deee05db5190a72a80d2db80a3f7c2708ee5
18875140516 <liu4242@qq.com>
2020-12-18 17:38:02 +0800

    init 0.1

---
+++ initvim.sh
@@ -0,0 +1,7 @@
+curl -sL install-node.now.sh/lts | sudo bash
+sudo add-apt-repository ppa:jonathonf/vim 
+sudo apt update
+sudo apt install vim
+vim +PluginInstall +qall
+
+
---
+++ initzsh.sh
@@ -0,0 +1,7 @@
+sudo apt-get install zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sudo sh
+chsh -s /bin/zsh
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+
