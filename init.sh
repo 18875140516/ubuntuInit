5e5aaffde12995608e07405f6bb2f55897762f8e
18875140516 <liu4242@qq.com>
2020-12-19 13:04:05 +0000

    delete initvim and initzsh

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ sudo chmod a+w /etc/hosts
 echo 199.232.96.133  raw.github.com >> /etc/hosts
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 sudo add-apt-repository ppa:jonathonf/vim 
 sudo add-apt-repository ppa:lazygit-team/release
 sudo apt update
@@ -12,7 +14,5 @@ curl -sL install-node.now.sh/lts | sudo bash
 curl -L -k  https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sudo sh
 vim +PluginInstall +qall
 chsh -s /bin/zsh
-zsh
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 
